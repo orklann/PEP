43fdfc69a17971476949d8ed81d174f02c602b2d
Aaron Elkins <threcius@yahoo.com>
2020-08-21 23:07:38 +0800

    Unit tests are ready

--- PEPTests/PEPTests.m
+++ PEPTests/PEPTests.m
@@ -25,13 +25,7 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+    XCTAssert(YES);
 }
 
 @end
