028822ea66a311a16f9dc311929cdc1cf7e9689d
Aaron Elkins <threcius@yahoo.com>
2020-08-23 11:22:51 +0800

    PEPTests: Remove testExample

--- PEPTests/PEPTests.m
+++ PEPTests/PEPTests.m
@@ -22,11 +22,4 @@
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-    XCTAssert(YES);
-}
-
 @end
