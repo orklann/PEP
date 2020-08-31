175b1d8807ae2b1452e6c8375c2150f291cf78f3
Aaron Elkins <threcius@yahoo.com>
2020-08-31 20:33:34 +0800

    GParser: add '\0' at the end of stream in @(setStream:)

--- PEP/Gene/GObjects.m
+++ PEP/Gene/GObjects.m
@@ -327,12 +327,7 @@
 
 - (void)parse {
     GParser *p = [GParser parser];
-    NSMutableData *d = [NSMutableData dataWithBytes:[rawContent bytes]
-                                             length:[rawContent length]];
-    // End stream with '\0' to ensure it will stop parsing
-    // Because GLexer need '\0' at the end to generate kEndToken
-    [d appendBytes:"\0" length:1];
-    [p setStream:d];
+    [p setStream:rawContent];
     [p parse];
     NSArray *array = [p objects];
     NSMutableDictionary *dict = [NSMutableDictionary dictionary];
@@ -371,12 +366,7 @@
 
 - (void)parse {
     GParser *p = [GParser parser];
-    NSMutableData *d = [NSMutableData dataWithBytes:[rawContent bytes]
-                                             length:[rawContent length]];
-    // End stream with '\0' to ensure it will stop parsing
-    // Because GLexer need '\0' at the end to generate kEndToken
-    [d appendBytes:"\0" length:1];
-    [p setStream:d];
+    [p setStream:rawContent];
     [p parse];
     value = [p objects];
 }
@@ -452,12 +442,7 @@
 
 - (void)parse {
     GParser *p = [GParser parser];
-    NSMutableData *d = [NSMutableData dataWithBytes:[rawContent bytes]
-                                             length:[rawContent length]];
-    // End stream with '\0' to ensure it will stop parsing
-    // Because GLexer need '\0' at the end to generate kEndToken
-    [d appendBytes:"\0" length:1];
-    [p setStream:d];
+    [p setStream:rawContent];
     [p parse];
     
     // Indirect object only contains one object, which is the first one
