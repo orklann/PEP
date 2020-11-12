59b8a210165b549bfd825de0092bd2f5932a4f3c
Aaron Elkins <threcius@yahoo.com>
2020-11-12 13:24:19 +0800

    GObjects: update [GHexStringObject parse]

--- PEP/Gene/GObjects.m
+++ PEP/Gene/GObjects.m
@@ -336,8 +336,9 @@ NSArray *getDynamicCommandArgs(NSArray *objects) {
         unsigned char ch2 = [self nextChar];
         NSString *s = [NSString stringWithFormat:@"%c%c", ch1, ch2];
         long result = strtol([s UTF8String], NULL, 16);
-        unsigned char v[2];
-        sprintf((char *)&v, "%c", (unsigned char)result);
+        unsigned char v = (unsigned char)result;
+        // TODO: Test for "bug_hex_string.pdf"
+        //sprintf((char *)&v, "%c", (unsigned char)result);
         [d appendBytes:&v length:1];
         [self nextChar];
     }
