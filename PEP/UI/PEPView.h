3a321f8fc5383a794241b67a55ceb791626ee63f
Aaron Elkins <threcius@yahoo.com>
2020-11-14 16:14:04 +0800

    introduce PEPView

---
+++ PEP/UI/PEPView.h
@@ -0,0 +1,17 @@
+//
+//  PEPView.h
+//  PEP
+//
+//  Created by Aaron Elkins on 11/14/20.
+//  Copyright © 2020 Aaron Elkins. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEPView : NSView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PEP/UI/PEPView.m
@@ -0,0 +1,22 @@
+//
+//  PEPView.m
+//  PEP
+//
+//  Created by Aaron Elkins on 11/14/20.
+//  Copyright © 2020 Aaron Elkins. All rights reserved.
+//
+
+#import "PEPView.h"
+
+@implementation PEPView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+    NSColor *bgColor = [NSColor colorWithRed:0.15 green:0.15 blue:0.15 alpha:1.0];
+    [bgColor set];
+    NSRectFill([self bounds]);
+}
+
+@end
