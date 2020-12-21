33caf34304613f38acc8e1dd7572502951a0d6fc
Aaron Elkins <threcius@yahoo.com>
2020-12-21 15:46:54 +0800

    GTextEditor: add comments

--- PEP/Gene/GTextEditor.m
+++ PEP/Gene/GTextEditor.m
@@ -210,7 +210,9 @@
 
 - (CGFloat)getEditorWidth {
     // 5 points margin on both sides
-    // NOTE: Remove 5 points margin on both side
+    // NOTE: Remove 5 points margin on both side, so that prevent wordwrapping change
+    //       text layout while enter new text (For example: Super Mario World.pdf)
+    //
     return editorWidth;// + (5 * 2);
 }
 
