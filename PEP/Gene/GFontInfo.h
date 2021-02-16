665a97744241e705543438a2d21f3f5828be8b93
Aaron Elkins <threcius@yahoo.com>
2021-02-16 14:41:31 +0800

    Introduce GFontInfo class

---
+++ PEP/Gene/GFontInfo.h
@@ -0,0 +1,23 @@
+//
+//  GFontInfo.h
+//  PEP
+//
+//  Created by Aaron Elkins on 2/16/21.
+//  Copyright © 2021 Aaron Elkins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GFontInfo : NSObject {
+    
+}
+
+@property (readwrite) unichar firstChar;
+@property (readwrite) NSMutableArray *widths;
+
++ (id)create;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PEP/Gene/GFontInfo.m
@@ -0,0 +1,16 @@
+//
+//  GFontInfo.m
+//  PEP
+//
+//  Created by Aaron Elkins on 2/16/21.
+//  Copyright © 2021 Aaron Elkins. All rights reserved.
+//
+
+#import "GFontInfo.h"
+
+@implementation GFontInfo
++ (id)create {
+    GFontInfo *o = [[GFontInfo alloc] init];
+    return o;
+}
+@end
