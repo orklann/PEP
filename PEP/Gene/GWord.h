731058bfa8e3d376a37c1e549ad026720e43e25a
Aaron Elkins <threcius@yahoo.com>
2021-03-08 13:39:19 +0800

    GWord: Add comment

--- PEP/Gene/GWord.h
+++ PEP/Gene/GWord.h
@@ -13,8 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /*
  * Indicate that we have no native distance between words, for example the last word in previous line
- * and first word in current line, we can add our own distance while doing
- * word wrapping.
+ * and first word in current line, we can add our own distance while doing word wrapping.
  */
 #define kNoWordDistance -1
 
@@ -23,7 +22,9 @@ NS_ASSUME_NONNULL_BEGIN
     NSRect frame;
 }
 
-/* word distance to prev word in text space */
+/*
+ * Word distance to prev word in text space, this property is only used in word wrapping in text parser
+ */
 @property (readwrite) CGFloat wordDistance;
 
 + (id)create;
