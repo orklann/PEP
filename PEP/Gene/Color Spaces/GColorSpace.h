d5cfe5da7cee16635c3b9506276d5880d5b7c38e
Aaron Elkins <threcius@yahoo.com>
2021-04-15 18:42:47 +0800

    GColorSpace: Modify comments

--- PEP/Gene/Color Spaces/GColorSpace.h
+++ PEP/Gene/Color Spaces/GColorSpace.h
@@ -22,7 +22,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (id)colorSpaceWithName:(NSString*)colorSpaceName page:(GPage*)page;
 
-/* No implementation, becuase we never call it, as a virtual function as like in C++ */
+/*
+ * No implementation, becuase we never call it, it works as a virtual function as like in C++.
+ * This method is just to make Xcode compiler happy.
+ */
 - (NSColor*)mapColor:(GCommandObject*)cmd;
 @end
 
